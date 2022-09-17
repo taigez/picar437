93177cb9fe8f674e4a48cca4ca06daa1922f71da
taigez <taigezhang@hotmail.com>
2022-09-17 11:14:53 -0500

    reboot

--- lab1/part2.py
+++ lab1/part2.py
@@ -15,14 +15,27 @@ curr_angle = -90
 max_angle = 90
 min_angle = -90
 
+CAR_COLOR = 100
+OBJECT_COLOR = 200
+RANGE_COLOR = 190
+
 # map car coord to matrix coord
-# ex: car starts at 0,0 but map to (50,80)
+# ex: car starts at 0,0 but map to (50,99)
 def to_x(x):
     return (int)(x+50)
 
 def to_y(y):
     return (int)(99-y)
 
+# map matrix coord to car coord
+# ex: (50,99) to (0,0)
+def from_x(x):
+    return x-50
+
+def from_y(y):
+    return 99-y
+
+# return a list of tuples with (current angle in real world, angle)
 def scan():
     global curr_dir, curr_angle
     output = []
@@ -36,8 +49,10 @@ def scan():
         elif curr_angle <= min_angle:
             curr_dir = step
             curr_angle = min_angle + (min_angle - curr_angle)
+    print(output)
     return output
-            
+
+# return list of (x,y) values rounded to nearest int
 def process(output):
     points = []
     for item in output:
@@ -46,14 +61,20 @@ def process(output):
         if distance <= 0:
             continue
         elif angle < 0:
-            x = distance*np.sin(-angle * np.pi/180)
-            y = distance*np.cos(-angle * np.pi/180)
-            points.append((np.round(x), np.round(y)))
+            x = np.round(distance*np.sin(-angle * np.pi/180))
+            y = np.round(distance*np.cos(-angle * np.pi/180))
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    points.append((x+i,y+j))
+            
+            
             
         else:
-            x = distance*np.sin(angle * np.pi/180)
-            y = distance*np.cos(angle * np.pi/180)
-            points.append((-np.round(x), np.round(y)))
+            x = -np.round(distance*np.sin(angle * np.pi/180))
+            y = np.round(distance*np.cos(angle * np.pi/180))
+            for i in range(-1,2):
+                for j in range(-1,2):
+                    points.append((x+i,y+j))
             
     return points
     
@@ -63,11 +84,11 @@ def to_color(x, y, A):
     if x < -50 or x > 50 or y < 0 or y > 99:
         return A
     else:
-        A[to_y(y)][to_x(x)] = 200
+        A[to_y(y)][to_x(x)] = OBJECT_COLOR
         return A
 
 A = np.zeros((y_height, x_width))
-A[to_y(0)][to_x(0)] = 100
+A[to_y(0)][to_x(0)] = CAR_COLOR
 
 points = process(scan())
 for point in points:
